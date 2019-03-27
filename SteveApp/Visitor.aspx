4fe45900e96afab63f192cfaa236fbff0154f8b6
maheshneerati <47025925+maheshneerati@users.noreply.github.com>
2019-03-27 11:55:09 +0530

    Add files via upload

---
+++ SteveApp/Visitor.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Visitor.aspx.cs" Inherits="Visitor" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+    </div>
+        <div>
+            <asp:Image ID="img" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ SteveApp/Visitor.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class Visitor : System.Web.UI.Page
+{
+    string imgurl = "http://localhost:63434/Images/beach-2179183_640.jpg";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        SubmitImg();
+    }
+    void SubmitImg()
+    {
+        img.ImageUrl = imgurl;
+    }
+}
